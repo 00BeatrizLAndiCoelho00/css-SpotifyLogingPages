eef777a082bc143681d939ad415d8f1c97412974
BeatrizLandi <andressa.fideliscoelho@gmail.com>
2023-01-31 20:07:36 -0300

    organized :)

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const http = require('http')
 const fs = require('fs')
 const port = 3000
 
+app.use(express.static('css'));
+
 const server = http.createServer(function(req, res) {
  res.writeHead(200, {'Content-Type': 'text/html'})
  fs.readFile('index.html', function(error, data){
